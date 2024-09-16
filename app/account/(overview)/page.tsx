05c2576071160c4272de156c9bd3a231eb86adff
Renzo Tomlinson <renzo.tomlinson@masterpoint.io>
2024-09-16 14:55:29 -0400

    account details page name

--- app/account/(overview)/page.tsx
+++ app/account/(overview)/page.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
 export default async function Page() {
   return (
     <main>
-      <h1 className={`mb-4 text-x1 md:text-2x1`}>Dashboard</h1>
+      <h1 className={`mb-4 text-x1 md:text-2x1`}>Account Details</h1>
       <h2>Your current balance is :</h2>
       <p>You can claim up to $500.00 per day.</p>
       <p>You have added $X.XX so far today.</p>
